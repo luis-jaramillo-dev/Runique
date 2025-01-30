9f99db35d48866e2e66ed713c410e954e0ad6199
Luis Fernando Jaramillo Sánchez <112969620+luis-jaramillo-dev@users.noreply.github.com>
2025-01-30 10:15:41 -0500

    4.7.3  Compose Convention Plugin
    Adding  Android Application Compose Convention Plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
+    alias(libs.plugins.runique.android.application.compose)
     alias(libs.plugins.mapsplatform.secrets.plugin)
-    alias(libs.plugins.runique.android.application)
 }
 
 android {
